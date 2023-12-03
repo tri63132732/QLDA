97ca5e51b3bb1217592eef94d7f07cd61a9fc3ca
NgoThanhDat_63133206 <dat.nth.63cntt@ntu.edu.vn>
2023-12-03 13:15:00 +0700

    Upload Empty Project
    Upload empty project mvc in Visual Studio 2022 with Authentication Invidual Account

---
+++ 4BroShop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_4BroShop.MvcApplication" Language="C#" %>
---
+++ 4BroShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_4BroShop.Startup))]
+namespace _4BroShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
