1e75c7efc10f29ca6869353654ea1426249ae38f
NgoThanhDat_63133206 <dat.nth.63cntt@ntu.edu.vn>
2023-12-04 20:27:27 +0700

    Customize Enity application user

---
+++ 4BroShop/Models/EFModels/Product.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _4BroShop.Models.EFModels
+{
+    [Table("tb_Product")]
+    public class Product : CommonAbstract
+    {
+        public Product()
+        {
+            this.ProductImage = new HashSet<ProductImage>();
+            this.OrderDetails = new HashSet<OrderDetail>();
+        }
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(250)]
+        public string Title { get; set; }
+
+        [StringLength(250)]
+        public string Alias { get; set; }
+
+        [StringLength(50)]
+        public string ProductCode { get; set; }
+        public string Description { get; set; }
+
+        [AllowHtml]
+        public string Detail { get; set; }
+
+        [StringLength(250)]
+        public string Image { get; set; }
+        //giá nhập
+        public decimal OriginalPrice { get; set; }
+        //giá bán
+        public decimal Price { get; set; }
+        //giá sau khi giảm (có thể null)
+        public decimal? PriceSale { get; set; }
+        //public int ViewCount { get; set; }
+        public bool IsHome { get; set; }
+        public bool IsSale { get; set; }
+        public bool IsFeature { get; set; }
+        public bool IsActive { get; set; }
+        public int ProductCategoryId { get; set; }
+
+        public virtual ProductCategory ProductCategory { get; set; }
+        public virtual ICollection<ProductImage> ProductImage { get; set; }
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; }
+    }
+}
--- 4BroShop/Models/IdentityModels.cs
+++ 4BroShop/Models/IdentityModels.cs
@@ -1,6 +1,7 @@
 ﻿using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using _4BroShop.Models.EFModels;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
@@ -24,7 +25,27 @@ namespace _4BroShop.Models
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
-
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //đổi tên bảng
+            modelBuilder.HasDefaultSchema("dbo");
+            modelBuilder.Entity<ApplicationUser>().ToTable("User");
+            modelBuilder.Entity<IdentityRole>().ToTable("Role");
+            modelBuilder.Entity<IdentityUserRole>().ToTable("UserRole");
+            modelBuilder.Entity<IdentityUserClaim>().ToTable("Claim");
+            modelBuilder.Entity<IdentityUserLogin>().ToTable("Login");
+            //đổi tên cột
+            /*modelBuilder.Entity<ApplicationUser>()
+               .Property(u => u.Id)
+               .HasColumnName("UserId");*/
+        }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
